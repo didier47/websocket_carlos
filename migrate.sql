edd9bece1ad6904a1c6fd7ced31353d858b24eec
didiergaona <didiergaona@wposs.com>
2021-09-22 21:44:03 -0500

    Configurado Dockerfile y docker-compose para lanzar bd y django

--- migrate.sql
+++ migrate.sql
@@ -1,23 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost:3306
--- Tiempo de generación: 22-09-2021 a las 23:40:12
--- Versión del servidor: 10.3.16-MariaDB
--- Versión de PHP: 7.3.23
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de datos: `id17622113_id16359616_sensor`
 --
@@ -139,8 +119,3 @@ INSERT INTO `datos` (`fecha`, `voltaje`, `corriente`) VALUES
 ('2021-09-20 01:41:26', 0.09, 0),
 ('2021-09-20 01:41:44', 0.09, 0),
 ('2021-09-20 01:42:01', 0.11, 0);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
