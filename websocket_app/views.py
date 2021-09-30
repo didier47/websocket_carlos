43d46b46a9aa1090f053edc75104a3a04c1e9d3c
didiergaona <didiergaona@wposs.com>
2021-09-30 13:57:43 -0500

    Quitado endpoint de borrar

--- websocket_app/views.py
+++ websocket_app/views.py
@@ -1,9 +1,14 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, ListModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from websocket_app.models import DataModel
 from websocket_app.serializers import DataSerializer
 
 
-class DataViewSet(ModelViewSet):
+class DataViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  UpdateModelMixin,
+                  ListModelMixin,
+                  GenericViewSet):
     queryset = DataModel.objects.all()
     serializer_class = DataSerializer
